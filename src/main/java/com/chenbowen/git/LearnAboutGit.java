485d95c7e575b2c3c02c44d14320d3ef7306758c
chenbowen <70129526.qq.com>
2019-02-24 16:05:33 +0800

    Resolve the conflict Test

--- src/main/java/com/chenbowen/git/LearnAboutGit.java
+++ src/main/java/com/chenbowen/git/LearnAboutGit.java
@@ -4,6 +4,7 @@ public class LearnAboutGit {
 
 	public static void main(String[] args) {
 		System.out.println("Learn About Git and GitHub");
-	}
+		System.out.println("Resolve the conflict Test... prosonOne");
+	} 
 	
 }
