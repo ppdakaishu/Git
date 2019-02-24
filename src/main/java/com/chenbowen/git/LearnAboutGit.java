c883992146daf349c566ffdc71a17cf2d2832250
chenbowen <70129526.qq.com>
2019-02-24 16:33:13 +0800

    delete System.out.println("hot_fix test"); on master

--- src/main/java/com/chenbowen/git/LearnAboutGit.java
+++ src/main/java/com/chenbowen/git/LearnAboutGit.java
@@ -6,7 +6,5 @@ public class LearnAboutGit {
 		System.out.println("Learn About Git and GitHub");
 		System.out.println("Resolve the conflict Test... personOne");
 		System.out.println("Resolve the conflict Test... personTwo fix");
-		System.out.println("hot_fix test");
 	} 
-	
 }
